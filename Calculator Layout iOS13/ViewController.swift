dcb5f138a65b4f8c95aed461b0d4c5ba24779542
Leon J Mac <leonjmac@protonmail.ch>
2020-04-21 18:58:39 +0100

    New skin applied with initial hooks to create a working calculator.

--- Calculator Layout iOS13/ViewController.swift
+++ Calculator Layout iOS13/ViewController.swift
@@ -10,11 +10,67 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var calcDisplay: UILabel!
+    var calcDisplayValue:String?
+    var functionKeyPressed = false
+    var prevValue = 0.0
+    var currentValue = 0.0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        resetCalculator()
     }
-
-
+    
+    @IBAction func functionKeyPressed(_ sender:UIButton) {
+        functionKeyPressed = true
+        applyFunction()
+        currentValue = 0.0
+    }
+    
+    @IBAction func numberKeyPressed(_ sender:UIButton) {
+        // unpack the number pressed
+        
+        // Invalidate functionkey pressed
+        functionKeyPressed = false
+        
+        // update display
+        
+    }
+    
+    @IBAction func modifierKeyPressed(_ sender:UIButton) {
+        guard let modifier = sender.currentTitle else {
+            return
+        }
+        
+        switch modifier {
+        case "%":
+            if (currentValue != 0.0) {
+                // Divide
+            }
+        case "AC":
+            resetCalculator()
+        case "+/-":
+            currentValue = -currentValue
+            refreshDisplay(withValue: currentValue)
+        default:
+            return
+        }
+    }
+    
+    func applyFunction() {
+        
+    }
+    
+    func refreshDisplay(withValue value:Double) {
+        self.prevValue = currentValue
+        // if already contains ".", prevent subsequent
+        
+    }
+    
+    func resetCalculator() {
+        prevValue = 0.0
+        currentValue = 0.0
+        functionKeyPressed = false
+        refreshDisplay(withValue: 0.0)
+    }
 }
-
